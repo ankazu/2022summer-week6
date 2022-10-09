ed519a484641f693de07038dabae2889e9bb645d
John <khgfe2002@gmail.com>
2022-10-09 13:09:33 +0800

    登入Model

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -16,7 +16,17 @@ var swiper2 = new Swiper(".giveBack", {
 
 const menuBtn = document.querySelector('.menu_btn');
 const menuNav = document.getElementsByTagName('nav')[0];
+const loginClose = document.getElementById('login_close');
+const loginModel = document.getElementById('login_modal');
+const signIn = document.getElementById('sign_in');
 
 menuBtn.addEventListener('click', function(e) {
   menuNav.classList.toggle('menu_open');
+})
+
+loginClose.addEventListener('click', function() {
+  loginModel.style.display = 'none';
+})
+signIn.addEventListener('click', function() {
+  loginModel.style.display = 'block';
 })
